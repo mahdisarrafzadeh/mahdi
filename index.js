467b244eb63f35693bfb2ba9eb81e5eeb540393c
Mahdi <mahdisarrafz9@gmail.com>
2021-02-25 12:12:01 +0330

    add view engine

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+
 app.get('/',(req, res) => {
-    res.send('Hello, word')
+   return res.render('index')
 })
 
 app.listen(port, () => {
