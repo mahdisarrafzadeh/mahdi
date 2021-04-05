61e4c8ff38267cb12d73279d77def4d8d7a7d5f9
Mahdi <mahdisarrafz9@gmail.com>
2021-04-05 18:48:02 +0430

    first

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-
 const express = require('express')
+const bodyParser = require('body-parser')
+require('./utils/db.config')
+
+const authRoutes=require('./routes/authRoutes')
 const app = express()
-const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.get('/',(req, res) => {
-   return res.render('index')
+app.use('/', authRoutes)
+
+
+app.get('/', (req, res) => {
+  return res.render('index')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+
+app.listen(3001, () => {
+  console.log('Server running at port 3000')
 })
 
-module.export = app
+module.exports = app
